5ae49a8f0a34b08d1ec2a490bfeac6fae07f6321
supremeashu <ashutoshmohapatra03@gmail.com>
2025-06-13 16:14:10 +0530

    Fix formatting of shift operations in barrel shifter

--- barrel_shifter/barrel_shifter.v
+++ barrel_shifter/barrel_shifter.v
@@ -8,12 +8,12 @@ module barrel_shifter #(parameter WIDTH=8)(
         case(shift_amt)
             3'b000: out = in;
             3'b001: out = {1'b0, in[7:1]};
-            3'b010: out = {{2{1'b0}}, in[7:2]};
-            3'b011: out = {{3{1'b0}}, in[7:3]};
-            3'b100: out = {{4{1'b0}}, in[7:4]};
-            3'b101: out = {{5{1'b0}}, in[7:5]};
-            3'b110: out = {{6{1'b0}}, in[7:6]};
-            3'b111: out = {{7{1'b0}}, in[7:7]};
+            3'b010: out = {{2{1'b0} }, in[7:2]};
+            3'b011: out = {{3{1'b0} }, in[7:3]};
+            3'b100: out = {{4{1'b0} }, in[7:4]};
+            3'b101: out = {{5{1'b0} }, in[7:5]};
+            3'b110: out = {{6{1'b0} }, in[7:6]};
+            3'b111: out = {{7{1'b0} }, in[7:7]};
         endcase
     end
 
